16e318e6d563f9d9d4a6c7cfafb2d8f4e1eb6d25
Kevin Maulana Atmaja <kevinmaulanaat@gmail.com>
2026-01-01 12:00:00 +0700

    style: format code using gofumpt

--- 01_tipe_data.go
+++ 01_tipe_data.go
@@ -22,39 +22,37 @@ func dataType() {
 	fmt.Println("City:", city)
 	fmt.Println("Year:", year)
 
-
 	fmt.Println("=============KONSTANTA=============")
 	// konstanta => nilai tak bisa diubah
 	// utk data yg sifatnya tetap, cth nama app, rumus, dll
 	const pi = 3.14
 	const appName = "Belajar Golang"
-	
+
 	fmt.Println("Nilai Pi:", pi)
 	fmt.Println("App Name:", appName)
-	
+
 	// jika coba diubah akan error
 	// pi = 3.1415
-
 
 	fmt.Println("=============NUMBER=============")
 	// tipe data number
 	// integer-> angka bulat
 	// floating point-> angka desimal
-	
+
 	// signed integers
 	var i8 int8 = 127
 	var i16 int16 = -32767
 	var i32 int32 = 2147483647
 	var i64 int64 = -9223372036854775807
 	var i int = -100
-	
+
 	// unsigned integers
 	var u8 uint8 = 127
 	var u16 uint16 = 32767
 	var u32 uint32 = 2147483647
 	var u64 uint64 = 9223372036854775807
 	var u uint = 100
-	
+
 	// printf-> utk print dg format
 	fmt.Println("Signed integers: ")
 	fmt.Printf("int8   : %v\n", i8)
@@ -62,50 +60,45 @@ func dataType() {
 	fmt.Printf("int32  : %v\n", i32)
 	fmt.Printf("int64  : %v\n", i64)
 	fmt.Printf("int    : %v\n", i)
-	
-	
+
 	fmt.Println("\nUnsigned integers: ")
 	fmt.Printf("uint8   : %v\n", u8)
 	fmt.Printf("uint16  : %v\n", u16)
 	fmt.Printf("uint32  : %v\n", u32)
 	fmt.Printf("uint64  : %v\n", u64)
-	fmt.Printf("uint    : %v\n", u)	
-
+	fmt.Printf("uint    : %v\n", u)
 
 	fmt.Println("=============FLOAT=============")
 	// floating point
 	var f32 float32 = 3.1415926535
 	var f64 float64 = 3.14159265358979323846
-	
+
 	fmt.Println("Nilai float32:", f32)
 	fmt.Println("Nilai float64:", f64)
-
 
 	fmt.Println("=============BOOL=============")
 	var isRaining bool = true
 	var isSunny bool = false
 	isLogedIn := true
 	hasPermission := false
-	
+
 	fmt.Println("Apakah hujan?", isRaining)
 	fmt.Println("Apakah cerah?", isSunny)
 	fmt.Println("Apakah login?", isLogedIn)
 	fmt.Println("Apakah punya akses?", hasPermission)
-
 
 	fmt.Println("=============STRING=============")
 	// string => kumpulan karakter yg ditulis diantara tanda kutip ganda("")/backtick(`)
 	// utk simpan dan manipulasi teks, imutable, slice of bytes
 	var nama string = "Amel"
 	pesan := "Amel Cantik!"
-	
+
 	paragraf := `Haloo ini adalah contoh 
 	multi-line string di Go.`
-	
+
 	fmt.Println("Nama:", nama)
 	fmt.Println("Pesan:", pesan)
 	fmt.Println("Paragraf:\n", paragraf)
-
 
 	fmt.Println("=============MANIPULASI STRING=============")
 	// manipulasi string
@@ -118,13 +111,13 @@ func dataType() {
 	fmt.Println(string("Go"[0]))
 	// text3 := "Go"[0] = 'g' -> error imutable
 
-	fmt.Println("Lowercase:", strings.ToLower(text)) // belajar golang
-	fmt.Println("Uppercase:", strings.ToUpper(text)) // BELAJAR GOLANG
+	fmt.Println("Lowercase:", strings.ToLower(text))                       // belajar golang
+	fmt.Println("Uppercase:", strings.ToUpper(text))                       // BELAJAR GOLANG
 	fmt.Println("StartsWith Belajar?", strings.HasPrefix(text, "Belajar")) //true
-	fmt.Println("Contains Golang?", strings.Contains(text, "Golang")) //true
+	fmt.Println("Contains Golang?", strings.Contains(text, "Golang"))      //true
 
 	parts := strings.Split("apel,banana,cerry", ",") //delimiter
-	fmt.Println("Split:", parts) //[apel banana cerry]
+	fmt.Println("Split:", parts)                     //[apel banana cerry]
 
 	newText := strings.ReplaceAll(text, "Belajar", "Halo")
 	fmt.Println("Replace:", newText) // Halo Golang!
--- 03_array.go
+++ 03_array.go
@@ -8,17 +8,16 @@ func array() {
 	// exp: 12 telur g bisa ditmbh/dikurangi
 	// index=> no urut utk akses elemen di array, slalu dimulai dari 0
 	var angka [3]int = [3]int{10, 20, 30}
-	fmt.Println(angka) //[10 20 30]
+	fmt.Println(angka)    //[10 20 30]
 	fmt.Println(angka[1]) //20
 	angka[2] = 60
 	fmt.Println("Angka yg telah diubah:", angka[2])
 
-
 	fmt.Println("=============FUNCTION ARRAY=============")
 	number := [5]int{10, 20, 30, 40, 50}
-	fmt.Println("Jumlah elemen:", len(number)) // [10 20 30 40 50]
-	fmt.Println("Index ke-2:",number[2]) //30 ->akses elemen
-	number[2] = 65 //ubah elemen di index ke-2
+	fmt.Println("Jumlah elemen:", len(number))       // [10 20 30 40 50]
+	fmt.Println("Index ke-2:", number[2])            //30 ->akses elemen
+	number[2] = 65                                   //ubah elemen di index ke-2
 	fmt.Println("Nilai yg telah diubah:", number[2]) //65
 
 	fmt.Println("Ini adalah aaray:")
@@ -26,8 +25,8 @@ func array() {
 		fmt.Println("Isi index ke:", index, "=", value)
 	}
 
-	arr1 := [3]int{1,2,3}
-	arr2 := [3]int{4,5,6}
+	arr1 := [3]int{1, 2, 3}
+	arr2 := [3]int{4, 5, 6}
 
 	fmt.Println("arr1 == arr2:", arr1 == arr2) //false
 	fmt.Println("arr1 != arr2:", arr1 != arr2) //true
